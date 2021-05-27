0422dfbf9eab58125963c30a11ebd319a4ded61e
Mohisha1108 <73828119+Mohisha1108@users.noreply.github.com>
2021-05-27 18:11:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX = 0;
+noseY = 0;
+function preload(){
+  mustache = loadImage('https://i.postimg.cc/zG9VRZHr/mus.png');
+}
+function setup(){
+  canvas = createCanvas(300,300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  posenet = ml5.poseNet(video,modelLoaded);
+  posenet.on('pose',gotPoses);
+}
+function gotPoses(result){
+  if (result.length > 0){
+    console.log(result)
+  noseX = result[0].pose.nose.x;
+  console.log("x-coordinate of nose = "+noseX);
+  noseY = result[0].pose.nose.y;
+  console.log("y-coordinate of nose = "+noseY);
+  }
+}
+function modelLoaded(){
+  console.log("Model is loaded");
+}
+function draw(){
+  image(video, 0,0,300,300);
+  //circle(noseX-175,noseY-105,20);
+  fill("red");
+  stroke("red");
+  image(mustache,noseX-220,noseY-110,100,50);
+}
+function mustache(){
+}
+function take_snapshot(){
+    save("my-filter-pic.png");;
+}
